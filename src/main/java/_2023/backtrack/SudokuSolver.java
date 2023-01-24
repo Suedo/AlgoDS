99af531c387c223de1d7bbc522bc30eb4dd0f0e6
snag <somjitnag192@gmail.com>
2023-01-25 01:41:14 +0530

    sudoku done n optimized

--- src/main/java/_2023/backtrack/SudokuSolver.java
+++ src/main/java/_2023/backtrack/SudokuSolver.java
@@ -1,29 +1,22 @@
 package _2023.backtrack;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class SudokuSolver {
 
     char dot = '.';
     char[] valids = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-    public int gridNum(int i, int j) {
-        return ((i / 3) * 3) + j / 3;
-    }
-
     // https://leetcode.com/problems/sudoku-solver/
     public void solveSudoku(char[][] board) {
-
         dfs(board, 0, 0);
-        print(board);
     }
 
-    private boolean isValid(char[][] board, int row, int col, Character option) {
+    private boolean isValid(char[][] board, int row, int col, char option) {
 
         for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == option) return false;
-        }
-
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == option) return false;
+            if (board[row][i] == option || board[i][col] == option) return false;
         }
 
         int subGridStartRow = row - row % 3;
@@ -37,16 +30,16 @@ public class SudokuSolver {
     }
 
     public boolean dfs(char[][] board, int row, int col) {
-        System.out.println(String.format("at [%d, %d]", row, col));
+        // System.out.println(String.format("at [%d, %d]", row, col));
 
         if (row == 9) return true; // solved
         if (col == 9) return dfs(board, row + 1, 0); // next row
         if (board[row][col] != dot) return dfs(board, row, col + 1);
 
-        for (Character option : valids) {
+        for (char option : valids) {
             if (isValid(board, row, col, option)) {
                 board[row][col] = option;
-                System.out.println(String.format("    [%d, %d], v: %s", row, col, option));
+                // System.out.println(String.format("    [%d, %d], v: %s", row, col, option));
                 if (dfs(board, row, col + 1)) return true;
                 board[row][col] = dot; // backtrack
             }
@@ -77,6 +70,12 @@ public class SudokuSolver {
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
         SudokuSolver sudokuSolver = new SudokuSolver();
+        Instant start = Instant.now();
         sudokuSolver.solveSudoku(board);
+        Instant stop = Instant.now();
+
+        sudokuSolver.print(board);
+        System.out.println("\n\n");
+        System.out.println("time taken (ms): " + Duration.between(start, stop).toMillis());
     }
 }
